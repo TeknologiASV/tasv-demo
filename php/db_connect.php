5bf229335160cbac8ba35a8543e08781d25dd9e0
speedjin1995 <speedjin1995@gmail.com>
2024-08-10 23:56:13 +0800

    fix login issues

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,5 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
+$db = mysqli_connect("tasv-database.czjdmxerbddi.ap-southeast-1.rds.amazonaws.com", "tasv", "PublikaTASV", "demo");
 $dbU = mysqli_connect("tasv-database.czjdmxerbddi.ap-southeast-1.rds.amazonaws.com", "tasv", "PublikaTASV", "uniqlo");
 $dbM = mysqli_connect("tasv-database.czjdmxerbddi.ap-southeast-1.rds.amazonaws.com", "tasv", "PublikaTASV", "Melaka");
 $dbI = mysqli_connect("tasv-database.czjdmxerbddi.ap-southeast-1.rds.amazonaws.com", "tasv", "PublikaTASV", "ijm");
