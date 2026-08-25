ac220545a9ae0d45831e620186fd6aa43df6a369
speedjin1995 <speedjin1995@gmail.com>
2026-08-25 14:30:34 +0800

    fix connection issues

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,10 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "demo");
-$dbU = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "uniqlo");
-$dbM = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "Melaka");
-$dbI = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "ijm");
-$dbF = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "felda");
+$db = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "tasvcom_demo");
+$dbU = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "tasvcom_uniqlo");
+$dbM = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "tasvcom_Melaka");
+$dbI = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "tasvcom_ijm");
+$dbF = mysqli_connect("localhost", "tasvcom_desight", "Tasv3319$", "tasvcom_felda");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
